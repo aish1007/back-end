2fd4d5998f1c23212104bceccd6949fde589f0a8
Aish1007 <aishwarya.narayanan025@gmail.com>
2023-04-02 17:09:35 -0700

    Prettify code

--- src/services/Match.ts
+++ src/services/Match.ts
@@ -2,17 +2,22 @@ import { MatchList } from "../interface/Match";
 
 const LeagueJS = require("leaguejs");
 const RIOT_API_KEY = process.env["RIOT_API_KEY"];
-const leagueJs = new LeagueJS(RIOT_API_KEY, {STATIC_DATA_ROOT: "./",limits: {allowBurst: true}});
+const leagueJs = new LeagueJS(RIOT_API_KEY, {
+  STATIC_DATA_ROOT: "./",
+  limits: { allowBurst: true },
+});
 
-export const getRecentMatches = async (playerId: string, envName: string): Promise<MatchList> => {
+export const getRecentMatches = async (
+  playerId: string,
+  envName: string
+): Promise<MatchList> => {
   const options = {
-    endIndex: 5
-  }
+    endIndex: 5,
+  };
   const matchList = await leagueJs.Match.gettingListByAccount(
     playerId,
     envName,
     options
   );
   return matchList;
-
 };
--- src/services/Summoner.ts
+++ src/services/Summoner.ts
@@ -2,7 +2,7 @@ import { Summoner } from "../interface/Summoner";
 
 const LeagueJS = require("leaguejs");
 const RIOT_API_KEY = process.env["RIOT_API_KEY"];
-const leagueJs = new LeagueJS(RIOT_API_KEY, {STATIC_DATA_ROOT: "./"});
+const leagueJs = new LeagueJS(RIOT_API_KEY, { STATIC_DATA_ROOT: "./" });
 
 export const getSummonerInfoByName = async (
   name: string,
